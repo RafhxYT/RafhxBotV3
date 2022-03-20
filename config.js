b52212359e102ae27d90c8c325fa676a0f2ca6d1
RafhxYT <85090530+RafhxYT@users.noreply.github.com>
2022-03-20 15:40:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 global.DeveloperMode = 'false' //true Or false
-global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
+global.linkGC = ['https://chat.whatsapp.com/Gup2iPdhIKx5Wo9Q7bVUN8']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
   '62857139649630': { // put your number here
@@ -38,8 +38,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot Is The Best'
+global.packname = 'wa.me/6281779434847'
+global.author = 'RafhxBotV3'
 
 global.multiplier = 39 // The higher, The harder levelup
 
